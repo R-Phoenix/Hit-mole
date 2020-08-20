db8b91f1c5879f0b81e9a4fbc8a2c428b0509bb4
R-Phoenix <64701895+R-Phoenix@users.noreply.github.com>
2020-08-20 19:04:52 +0530

    Add files via upload

--- mole.js
+++ mole.js
@@ -1,26 +1,33 @@
 hearts = 5;
+t=500;
+b=0;
 point=0;
 var timer = setInterval(function() {
     if (hearts <= 0){
         clearInterval(timer);
     }
+    if (b>=120000){
+        clearInterval(timer);
+    }
     y=getRnd(1,9);
     var p = document.getElementById(y);
     p.src="mc.jpg";
+    b+=800;
+    console.log(b);
+    t=500+(getRnd(1,5)*100);
     p.style.opacity=1;
     setTimeout(() => {
         p.src="hole.jpg";
         p.style.opacity=0.9;
-    }, 600);
+    }, t);
     document.getElementById("p").innerHTML=point;
     document.getElementById("h").innerHTML=hearts;
-}, 1000);
+}, 800);
 function hit (i){
     if (hearts==0){
         return;
     }
     var x=document.getElementById(i).style.opacity;
-    console.log("true");
     if (x == 1){
         point+=1;
     }
